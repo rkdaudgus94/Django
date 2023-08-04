4fe6ed8cb3abe2423134e566d873e324fd9c8835
강명현 <rkdaudgus2650@gmail.com>
2023-08-04 16:11:09 +0900

    포스트 목록 페이지에 부트스트랩 적용

--- blog/views.py
+++ blog/views.py
@@ -18,14 +18,14 @@ class PostList(ListView):
 
 class PostDetail(DetailView):
     model = Post
-def single_post_page(request, pk) :
-    post = Post.objects.get(pk=pk)
-
-    return render(
-        request,
-        'blog/single_post_page.html',
-        {
-            'post' : post,
-        }
-    )
+# def single_post_page(request, pk) :
+#     post = Post.objects.get(pk=pk)
+#
+#     return render(
+#         request,
+#         'blog/post_detail.html',
+#         {
+#             'post' : post,
+#         }
+#     )
 
