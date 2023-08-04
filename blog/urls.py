12eb2cee63a17c6306414bebae723e41f26f67e4
강명현 <rkdaudgus2650@gmail.com>
2023-08-04 14:36:07 +0900

    .

--- blog/urls.py
+++ blog/urls.py
@@ -3,9 +3,7 @@ from . import views
 from django.contrib import admin
 
 urlpatterns = [
-    path('about_me/', views.about_me),
-    path('', views.landing),
     path('<int:pk>/', views.single_post_page),
     path('',views.index),
-    path('', include('single_pages.urls'))
+
 ]
