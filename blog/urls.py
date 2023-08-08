b6aa65c28c208af86936bccb8db0f5544beabfdf
강명현 <rkdaudgus2650@gmail.com>
2023-08-08 14:16:13 +0900

    카테고리 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from . import views
 from django.contrib import admin
 
 urlpatterns = [
+    path('category/<str:slug>/', views.category_page),
     path('<int:pk>/', views.PostDetail.as_view()),
     path('', views.PostList.as_view()),
     # path('<int:pk>/', views.single_post_page),
