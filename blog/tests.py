4a0db5f8c099ce2db45a0aa0b68344ee3d164a4c
강명현 <rkdaudgus2650@gmail.com>
2023-08-08 09:41:39 +0900

    포스트 목록, 상세 페이지에서 작성자 정보 출력하기

--- blog/tests.py
+++ blog/tests.py
@@ -1,10 +1,14 @@
 from django.test import TestCase, Client
 from bs4 import BeautifulSoup
+from django.contrib.auth.models import User
 from .models import Post
 # Create your tests here.
 class TestView(TestCase):
     def setUp(self):
         self.client = Client()
+        self.user_trump =User.objects.create_user(username='trump',password='somepassword')
+        self.user_obama =User.objects.create_user(username='obama',password='somepassword')
+
     def navbar_test(self, soup):
         navbar = soup.nav
         self.assertIn('Blog', navbar.text)
@@ -37,10 +41,13 @@ class TestView(TestCase):
         post_001 = Post.objects.create(
             title='첫 번째 포스트입니다.',
             content='Hello World. We are the world.',
+            author=self.user_trump
         )
         post_002 = Post.objects.create(
             title='두 번째 포스트입니다.',
             content='1등이 전부는 아니잖아요 ㅋ',
+            author=self.user_obama
+
         )
         self.assertEqual(Post.objects.count(), 2)
 
@@ -52,6 +59,8 @@ class TestView(TestCase):
         self.assertIn(post_002.title, main_area.text)
 
         self.assertNotIn('아직 게시물이 없습니다.', main_area.text)
+        self.assertIn(self.user_trump.username.upper(), main_area.text)
+        self.assertIn(self.user_obama.username.upper(), main_area.text)
 
 
     def test_post_detail(self):
@@ -59,12 +68,12 @@ class TestView(TestCase):
         post_001 = Post.objects.create(
             title='첫 번째 포스트입니다.',
             content='Hello World. We are the world.',
+            author=self.user_trump,
         )
         self.assertEqual(post_001.get_absolute_url(), '/blog/1/')
         response = self.client.get(post_001.get_absolute_url())
         self.assertEqual(response.status_code, 200)
         soup = BeautifulSoup(response.content, 'html.parser')
-
         self.navbar_test(soup)
 
         self.assertIn(post_001.title, soup.title.text)
@@ -72,6 +81,6 @@ class TestView(TestCase):
         main_area = soup.find('div', id='main-area')
         post_area = main_area.find('div', id='post-area')
         self.assertIn(post_001.title, post_area.text)
-
+        self.assertIn(self.user_trump.username.upper(), post_area.text)
         self.assertIn(post_001.content, post_area.text)
 
