a1550826619a04f12e61aa63ac64ea3b42f7fc24
강명현 <rkdaudgus2650@gmail.com>
2023-08-09 11:08:17 +0900

    포스트 작성 페이지 접근 권한 설정하기

--- blog/tests.py
+++ blog/tests.py
@@ -10,6 +10,8 @@ class TestView(TestCase):
         self.client = Client()
         self.user_trump =User.objects.create_user(username='trump',password='somepassword')
         self.user_obama =User.objects.create_user(username='obama',password='somepassword')
+        self.user_obama.is_staff = True
+        self.user_obama.save()
 
         self.category_programming = Category.objects.create(name='programming', slug='programming')
         self.category_music = Category.objects.create(name='music', slug='music')
@@ -169,7 +171,10 @@ class TestView(TestCase):
         self.assertNotEquals(response.status_code, 200)
 
         self.client.login(username='trump', password='somepassword')
+        response = self.client.get('/blog/create_post/')
+        self.assertNotEqual(response.status_code, 200)
 
+        self.client.login(username='obama', password='somepassword')
         response = self.client.get('/blog/create_post/')
         self.assertEqual(response.status_code, 200)
         soup = BeautifulSoup(response.content, 'html.parser')
@@ -188,6 +193,6 @@ class TestView(TestCase):
         self.assertEqual(Post.objects.count(), 4)
         last_post = Post.objects.last()
         self.assertEqual(last_post.title, "Post Form 만들기")
-        self.assertEqual(last_post.author.username, 'trump')
+        self.assertEqual(last_post.author.username, 'obama')
 
 
